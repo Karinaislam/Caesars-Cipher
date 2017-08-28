66f2f225bbde502c85374868e440859a74f852eb
Karina.I <badhon12karina@gmail.com>
2017-08-27 19:15:09 -0500

    Add files via upload

---
+++ source.js
@@ -0,0 +1,23 @@
+function input(){
+
+  var str = document.getElementById('input').value;
+  var output = rot13(str);
+  document.getElementById('output').innerHTML= output;
+}
+
+
+function rot13(str) {
+  
+  return str.split('').map.call(str, function(char) {
+      x = char.charCodeAt(0);
+      if (x < 65 || x > 90) {
+        return String.fromCharCode(x);  
+      }
+
+      else if (x < 78) {
+        return String.fromCharCode(x + 13);
+      }
+      
+      return String.fromCharCode(x - 13);
+    }).join('');  
+}
